ffdf4921f3adaa2d035596bb9fe291f3772f418d
Na._.kyung <lnk5324@naver.com>
2023-07-01 15:54:55 +0900

    23-07-01 로그인 페이지 create
    버튼 눌렀을때 대충 js랑 메뉴바 커서 올렸을때 커서 모양 뜨고 초록색으로 바뀌게 적용함. 아마 폰트 적용 빼고는 완벽한듯? 이 다음 페이지 만들면 그 페이지랑 연동해야함.

---
+++ loginPage/index.js
@@ -0,0 +1,16 @@
+var loginButton = document.getElementById('login-button');
+
+function validateForm() {
+    var username = document.querySelector('input[name="ID"]').value;
+    var password = document.querySelector('input[name="PW"]').value;
+
+    if (username === "" || password === "") {
+      alert("아이디와 비밀번호를 입력해주세요.");
+      return false;
+    }
+
+    else{ 
+        alert("로그인 성공");
+    }
+
+  }
