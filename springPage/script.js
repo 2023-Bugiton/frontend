50d14ebca2c19c8ecc77bc6cf28936743bb8b0e6
Na._.kyung <lnk5324@naver.com>
2023-07-04 11:51:55 +0900

    23-07-04 옵션 페이지 연결

--- springPage/script.js
+++ springPage/script.js
@@ -1,19 +1,23 @@
-document.getElementById('itemSelect').addEventListener('change', function() {
+/*document.getElementById('itemSelect').addEventListener('change', function() {
     var selectedValue = this.value;
     switch (selectedValue) {
     case 'opt1':
-        window.location.href = '#';
+        window.location.href = 'https://songPage/index.html';
         break;
     case 'opt2':
-        window.location.href = '#';
+        window.location.href = 'songPage/index.html';
         break;
     case 'opt3':
-        window.location.href = '#';
+        window.location.href = 'https://songPage/index.html';
         break;
     default:
         break;
     }
-});
+});*/
+
+function moveurl(url) { 
+    location.href = url;
+} 
 
 
 
