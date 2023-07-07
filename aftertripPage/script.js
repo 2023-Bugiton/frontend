f55681c7e035a5aa23bfb896a46777d5dbc1a25c
Sengoo <lesley023@naver.com>
2023-07-07 22:32:24 +0900

    23-07-07
    aftertripPage, mytewntysummerPage hover 기능 및 a태그, 별점 js 완성

--- aftertripPage/script.js
+++ aftertripPage/script.js
@@ -3,12 +3,32 @@ function addUnderline(element) {
   }
   
   function replaceImage(element) {
-    var originalSrc = 'images/ph_star-bold.png'; // 원래 이미지 소스
-    var newSrc = 'images/pull-star.png'; // 교체될 이미지 소스
+    var stars = element.parentNode.parentNode.getElementsByClassName('stars');
+    var index = Array.prototype.indexOf.call(stars, element.parentNode);
   
-    if (element.getAttribute('src') === originalSrc) {
-      element.setAttribute('src', newSrc); // 이미지를 교체될 이미지로 변경
+    var cnt = 0;
+    for (var i = 0; i < stars.length; i++) {
+      var star = stars[i].firstElementChild;
+      if (star.src.includes("pull-star.png")) {
+        cnt++;
+      }
+    }
+  
+    if (cnt == index + 1) {
+      for (var i = 0; i < stars.length; i++) {
+        var star = stars[i].firstElementChild;
+        star.src = "images/ph_star-bold.png";
+      }
     } else {
-      element.setAttribute('src', originalSrc); // 이미지를 원래 이미지로 복원
+      for (var i = 0; i <= index; i++) {
+        var star = stars[i].firstElementChild;
+        star.src = "images/pull-star.png";
+      }
+  
+      for (var i = index + 1; i < stars.length; i++) {
+        var star = stars[i].firstElementChild;
+        star.src = "images/ph_star-bold.png";
+      }
     }
   }
+  
--- mytwentysummer/script.js
+++ mytwentysummer/script.js
@@ -5,15 +5,29 @@ function addUnderline(element) {
 function replaceImage(element) {
   var stars = element.parentNode.parentNode.getElementsByClassName('stars');
   var index = Array.prototype.indexOf.call(stars, element.parentNode);
-  
 
-  for (var i = 0; i <= index; i++) {
+  var cnt = 0;
+  for (var i = 0; i < stars.length; i++) {
     var star = stars[i].firstElementChild;
-    star.src = "images/pull-star.png";
+    if (star.src.includes("pull-star.png")) {
+      cnt++;
+    }
   }
-  
-  for (var i = index + 1; i < stars.length; i++) {
-    var star = stars[i].firstElementChild;
-    star.src = "images/ph_star-bold.png";
+
+  if (cnt == index + 1) {
+    for (var i = 0; i < stars.length; i++) {
+      var star = stars[i].firstElementChild;
+      star.src = "images/ph_star-bold.png";
+    }
+  } else {
+    for (var i = 0; i <= index; i++) {
+      var star = stars[i].firstElementChild;
+      star.src = "images/pull-star.png";
+    }
+
+    for (var i = index + 1; i < stars.length; i++) {
+      var star = stars[i].firstElementChild;
+      star.src = "images/ph_star-bold.png";
+    }
   }
 }
